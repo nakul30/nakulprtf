49ba2bc8d9f010612d7f10957cd4fce1f8d63b74
Nakul <nakulgarg1424@gmail.com>
2024-06-29 02:07:32 +0530

    commit

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const projects = [
+  {
+    title: "Urbanmart",
+    subtitle: "MERN Stack",
+    description:
+      "Application that allows users to buy and sell products. Implementing Cart and Payment Gateway for easy payments .",
+    image: "./project-1.gif",
+    link: "",
+  },
+  // {
+  //   title: "React Tracks",
+  //   subtitle: "React and Python",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-2.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+  {
+    title: "Roommate Finder",
+    subtitle: "React and Firebase",
+    description:
+      "This is a full-stack application that helps you find roommates in your area. It uses React for the frontend and Firebase for the backend.",
+    image: "./project-3.gif",
+    link: "",
+  },
+  // {
+  //   title: "Epic Todo App",
+  //   subtitle: "React Hooks",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-4.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Data Structures & Algorithms",
+  "JavaScript",
+  "React",
+  "Node",
+  "Express",
+  "MongoDB",
+  "Firebase",
+  "Python",
+  "Redux",
+  "Git",
+  
+
+];
